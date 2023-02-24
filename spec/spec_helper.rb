21f5d841ca57d34e11cca47ebdd14c9193df1049
Valentin Kiselev <mrexox@evilmartians.com>
2023-02-24 14:05:05 +0300

    chore: add coverage (#5)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,22 @@
-require 'bundler/setup'
-require 'sentry/sanitizer'
-
 if ENV['CI'] == 'true'
   require 'simplecov'
-  require 'codecov'
+  require 'simplecov-lcov'
+  SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+  SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+  SimpleCov.start do
+    add_filter '/spec/'
+    enable_coverage :branch
+  end
 
-  SimpleCov.start
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  # require 'simplecov'
+  # require 'codecov'
+
+  # SimpleCov.start
+  # SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
+
+require 'bundler/setup'
+require 'sentry/sanitizer'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
