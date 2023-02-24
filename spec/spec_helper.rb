e9de170e86b6ece662c7a697f0adef8ec9f887db
Valentin Kiselev <mrexox@evilmartians.com>
2023-02-24 12:52:07 +0300

    chore: add lcov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 require 'simplecov'
+require 'simplecov-lcov'
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start do
   enable_coverage :branch
 end
