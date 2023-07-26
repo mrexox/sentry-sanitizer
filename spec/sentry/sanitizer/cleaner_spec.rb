87a20e92abf93c2e7551f1691677d93021631221
Valentin Kiselev <mrexox@evilmartians.com>
2023-07-26 15:55:25 +0300

    test: drop 4 tests

--- spec/sentry/sanitizer/cleaner_spec.rb
+++ spec/sentry/sanitizer/cleaner_spec.rb
@@ -97,79 +97,6 @@ RSpec.describe Sentry::Sanitizer::Cleaner do
       Sentry.get_current_scope.apply_to_event(event)
     end
 
-    context "with event as symbolized Hash" do
-      it "filters everything according to configuration" do
-        event_h = event.to_hash
-        subject.call(event_h)
-
-        expect(event_h).to match a_hash_including(
-          request: a_hash_including(
-            data: a_hash_including(
-              "password" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-              "secret_token" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-              "oops" => "OOPS",
-              "hmm" => [
-                a_hash_including(
-                  "password" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-                  "array" => "too"
-                )
-              ]
-            ),
-            headers: a_hash_including(
-              "H-1" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-              "H-2" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-              "H-3" => "secret3",
-              "Authorization" => "token",
-              "X-Xsrf-Token" => "xsrf=token"
-            ),
-            cookies: a_hash_including(
-              "cookie1" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-              "cookie2" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-              "cookie3" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK
-            )
-          ),
-          extra: a_hash_including(
-            password: Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-            not_password: "NOT SECRET"
-          )
-        )
-      end
-    end
-
-    context "with raw event" do
-      it "filters everything according to configuration" do
-        subject.call(event)
-
-        expect(event.request.data).to match a_hash_including(
-          "password" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-          "secret_token" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-          "oops" => "OOPS",
-          "hmm" => [
-            a_hash_including(
-              "password" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-              "array" => "too"
-            )
-          ]
-        )
-        expect(event.request.headers).to match a_hash_including(
-          "H-1" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-          "H-2" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-          "H-3" => "secret3",
-          "Authorization" => "token",
-          "X-Xsrf-Token" => "xsrf=token"
-        )
-        expect(event.request.cookies).to match a_hash_including(
-          "cookie1" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-          "cookie2" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-          "cookie3" => Sentry::Sanitizer::Cleaner::DEFAULT_MASK
-        )
-        expect(event.extra).to match a_hash_including(
-          password: Sentry::Sanitizer::Cleaner::DEFAULT_MASK,
-          not_password: "NOT SECRET"
-        )
-      end
-    end
-
     context "cleaning all headers" do
       it "filters everything according to configuration" do
         Sentry.get_current_client.configuration.sanitize.http_headers = true
@@ -267,32 +194,6 @@ RSpec.describe Sentry::Sanitizer::Cleaner do
       )
 
       Sentry.get_current_scope.apply_to_event(event)
-    end
-
-    it "uses given mask" do
-      event_h = JSON.parse(event.to_hash.to_json)
-      subject.call(event_h)
-
-      expect(event_h).to match a_hash_including(
-        "request" => a_hash_including(
-          "headers" => a_hash_including(
-            "Custom-header" => mask,
-            "Custom-nonsecure" => "NONSECURE",
-            "Authorization" => "token",
-            "X-Xsrf-Token" => "xsrf=token"
-          ),
-          "cookies" => a_hash_including(
-            "cookie1" => mask,
-            "cookie2" => mask,
-            "cookie3" => mask
-          ),
-          "query_string" => "password=#{mask}&token=#{mask}&nonsecure=NONESECURE&nested[][password]=#{mask}&nested[][login]=LOGIN"
-        ),
-        "extra" => a_hash_including(
-          "password" => mask,
-          "not_password" => "NOT SECRET"
-        )
-      )
     end
   end
 end
